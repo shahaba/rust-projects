88cb6dc1996ebb9fd06993051ddf06e72d1c3664
shahab <shahab.uweng@gmail.com>
2024-04-21 12:50:50 -0400

    Add random vector generator

--- rust-algos/search-algos/src/bubble_sort.rs
+++ rust-algos/search-algos/src/bubble_sort.rs
@@ -1,3 +1,5 @@
+use rand::Rng;
+
 pub fn bubble_sort(vec: &mut Vec<i32>) {
     let mut left = 0;
     let n = vec.len();
@@ -29,35 +31,56 @@ pub fn optimized_bubble_sort(vec: &mut Vec<i32>) {
     }
 }
 
+pub fn check_sorted(vec: &Vec<i32>) -> bool {
+    for index in 1..vec.len() {
+        if vec[index - 1] > vec[index] {
+            return false;
+        }
+    }
+    true
+}
+
+pub fn make_random_vec(num_items: i32, max: i32) -> Vec<i32> {
+    let mut rng = rand::thread_rng();
+
+    let mut vec: Vec<i32> = Vec::with_capacity(num_items as usize);
+
+    for _ in 0..num_items {
+        vec.push(rng.gen_range(0..max));
+    }
+
+    vec
+}
+
 #[cfg(test)]
 mod test {
-    use crate::bubble_sort::{bubble_sort, optimized_bubble_sort};
+    use crate::bubble_sort::{bubble_sort, check_sorted, make_random_vec, optimized_bubble_sort};
 
     #[test]
-    fn test_ordered_list() {
-        let mut arr = vec![1, 2, 3, 4];
+    fn test_random_list() {
+        let mut arr = make_random_vec(10, 30);
         bubble_sort(&mut arr);
-        assert_eq!(arr, vec![1, 2, 3, 4])
+        assert!(check_sorted(&arr))
     }
 
     #[test]
     fn test_reversed_list() {
         let mut arr = vec![4, 3, 2, 1];
         bubble_sort(&mut arr);
-        assert_eq!(arr, vec![1, 2, 3, 4])
+        assert!(check_sorted(&arr))
     }
 
     #[test]
-    fn test_ordered_list_optimized() {
-        let mut arr = vec![1, 2, 3, 4];
+    fn test_random_list_optimized() {
+        let mut arr = make_random_vec(10, 30);
         optimized_bubble_sort(&mut arr);
-        assert_eq!(arr, vec![1, 2, 3, 4])
+        assert!(check_sorted(&arr))
     }
 
     #[test]
     fn test_reversed_list_optimized() {
         let mut arr = vec![4, 3, 2, 1];
         optimized_bubble_sort(&mut arr);
-        assert_eq!(arr, vec![1, 2, 3, 4])
+        assert!(check_sorted(&arr))
     }
 }
